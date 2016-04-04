da79641136586756a85450e0f5726e9f4bc2e6be
Jester Lind <jester@jesterlind.com>
2016-04-04 17:56:55 -0400

    Bootstrap CDN
    Bootstrap CDN - Latest compiled and minified JavaScript

--- index.php
+++ index.php
@@ -60,7 +60,8 @@
 </table>
 </div>
 <?php include('includes/footer.html'); ?>
-
+<!-- Bootstrap CDN - Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 <script src="js/utility.js"></script>
 <script src="js/foundation.js"></script>
 </body>
